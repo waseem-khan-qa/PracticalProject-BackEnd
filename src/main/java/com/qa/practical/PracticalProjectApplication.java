ae82d3ed94e8551bab362a67402d4cbecb4bd2c9
waseem-khan-qa <mwk_33@hotmail.com>
2022-07-14 12:52:51 +0100

    create a project

---
+++ src/main/java/com/qa/practical/PracticalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.practical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/practical/PracticalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.practical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
